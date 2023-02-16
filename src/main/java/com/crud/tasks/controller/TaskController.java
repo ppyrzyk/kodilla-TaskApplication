6bfc9d098fe5d459fd3c934bc18aa0b4a9d971cc
Pawel Pyrzyk <ppyrzyk@gmail.com>
2023-02-16 13:55:40 +0100

    module 20.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -14,6 +14,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/tasks")
 @RequiredArgsConstructor
+@CrossOrigin("*")
+
 public class TaskController {
 
     private final DbService service;
