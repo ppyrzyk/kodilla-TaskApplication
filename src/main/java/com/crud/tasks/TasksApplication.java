204a1c86818c5edc20570aa08f76bb83b4fa8d51
ppyrzyk <115031262+ppyrzyk@users.noreply.github.com>
2023-04-03 12:26:25 +0200

    module 23.4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @OpenAPIDefinition
 @SpringBootApplication
 public class TasksApplication {
